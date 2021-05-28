a2a1a9aee69f33f3f2b5d2cc845d84c356a1a886
Alben <alurquiza@nauta.cu>
2021-05-27 21:54:09 -0400

    try implementation jwt

---
+++ CR_Backend/Controllers/LoginController.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Net;
+using System.Threading;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+using CR_Backend.Models;
+using CR_Backend.Security;
+
+using Microsoft.AspNetCore.Mvc;
+using HttpContext.Current.User;
+using Thread.CurrentPrincipal;
+
+namespace CR_Backend.Controllers  
+{
+    /// <summary>
+    /// login controller class for authenticate users
+    /// </summary>
+    [AllowAnonymous]
+    [RoutePrefix("api/login")]
+    public class LoginController : ControllerBase
+    {
+        [HttpGet]
+        [Route("echoping")]
+        public IHttpActionResult EchoPing()
+        {
+            return Ok(true);
+        }
+
+        [HttpGet]
+        [Route("echouser")]
+        public IHttpActionResult EchoUser()
+        {
+            var identity = Thread.CurrentPrincipal.Identity;
+            return Ok($" IPrincipal-user: {identity.Name} - IsAuthenticated: {identity.IsAuthenticated}");
+        }
+
+        [AllowAnonymous]
+        // [Route("authenticate")]
+        [HttpPost]
+        public IHttpActionResult Authenticate(LoginRequest login)
+        {
+            if (login == null)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            //TODO: Validate credentials Correctly, this code is only for demo !!
+            bool isCredentialValid = (login.Password == "123456");
+            if (isCredentialValid)
+            {
+                var token = TokenGenerator.GenerateTokenJwt(login.Username);
+                return Ok(token);
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+    }
+}
